8c2e6a9e805e24ac0aa156f3c1e2454170b121a4
Vastvik2106 <saranshbachchan2106@gmail.com>
2026-08-22 15:34:30 +0000

    Add Base64 decoder for text input
    Currently supports decoding text-based Base64 input only.
    Does not yet support arbitrary binary data, images, or other file types.

---
+++ Decoder.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string>
+#include <cstdint>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int final(vector<long long>&v){
+    int ans = 0;
+    int j = 0;
+    for(int i=v.size()-1; i>=0; i--){
+        ans = ans + (v[i]*pow(2,j));
+        j++;
+    }
+
+    return ans;
+
+}
+
+
+
+void  bin(vector<int>&v){
+    vector<long long> temp;
+    for(int i=0; i<v.size();i++){
+        int yum = v[i];
+        
+        vector<long long>aisa;
+        while(yum>0){
+            aisa.push_back(yum%2);
+            yum = yum/2;               
+        }
+
+        while(aisa.size()<6){
+            aisa.push_back(0);
+        }
+        for(int i=0; i<aisa.size();i++){
+            temp.push_back(aisa[aisa.size()-1-i]);
+        }       
+    }
+    for (int i = 0; i < temp.size(); i += 8) {
+        vector<long long> ouu(temp.begin() + i, temp.begin() + i + 8);
+        int result = final(ouu);
+        cout<<(char)result;
+    }
+    cout<<endl;
+    
+}
+
+
+
+void Decode(string s){
+    vector<int>v;
+    vector<string>o;
+    string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    for(int i=0; i<s.size();i++){
+        int value = alphabet.find(s[i]);
+        v.push_back(value);
+    }
+    
+    bin(v);
+
+}
+
+
+int main(){
+    string s;
+    getline(cin,s);
+
+    Decode(s);
+
+}
